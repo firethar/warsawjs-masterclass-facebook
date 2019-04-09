783b53704f12e85b2ccbdca62a8a4e853d44d0a8
firethar <firethar@gmail.com>
2019-04-09 16:00:07 +0200

    feat(posts): enable OnPush strategy in changeDetector

--- src/app/posts/components/post-list-item/post-list-item.component.ts
+++ src/app/posts/components/post-list-item/post-list-item.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-post-list-item',
   templateUrl: './post-list-item.component.html',
-  styleUrls: ['./post-list-item.component.scss']
+  styleUrls: ['./post-list-item.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PostListItemComponent implements OnInit {
 
