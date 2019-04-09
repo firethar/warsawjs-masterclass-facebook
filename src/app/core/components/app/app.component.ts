8096a1b84a7666a92acea67227244cce1037d5ae
firethar <firethar@gmail.com>
2019-04-09 11:25:20 +0200

    refactor: move app module

--- src/app/core/components/app/app.component.ts
+++ src/app/core/components/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'core-root',
+  selector: 'app-root',
   templateUrl: './core.component.html',
   styleUrls: ['./core.component.scss']
 })
-export class CoreComponent {
+export class AppComponent {
   title = 'Warsawjs Masterclass Facebook';
 }
