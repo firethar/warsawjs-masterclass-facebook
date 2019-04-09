377aa7b799b7020681f62e0634ef9b9e5a1d3ecf
firethar <firethar@gmail.com>
2019-04-09 18:13:39 +0200

    fix(tests): update page title

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to warsawjs-masterclass-facebook!');
+    expect(page.getTitleText()).toEqual('Facebook');
   });
 
   afterEach(async () => {
