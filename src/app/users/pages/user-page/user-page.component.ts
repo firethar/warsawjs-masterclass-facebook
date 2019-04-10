3240a0d1de9f7e7ed7a453647de98fd31d5deb45
firethar <firethar@gmail.com>
2019-04-10 16:40:45 +0200

    chore(performance): unsubscribe with takeUntil

--- src/app/users/pages/user-page/user-page.component.ts
+++ src/app/users/pages/user-page/user-page.component.ts
@@ -1,18 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { UsersService } from '../../services/users.service';
 import { ActivatedRoute } from '@angular/router';
 
 import { User } from '../../interfaces/user.interface';
+import { Subject } from 'rxjs';
+import { take, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-user-page',
   templateUrl: './user-page.component.html',
   styleUrls: ['./user-page.component.scss']
 })
-export class UserPageComponent implements OnInit {
+export class UserPageComponent implements OnInit, OnDestroy {
 
   user: User = null;
   error = null;
+
+  destroy$ = new Subject();
 
   constructor(
     private route: ActivatedRoute,
@@ -23,10 +27,17 @@ export class UserPageComponent implements OnInit {
     this.setupUser();
   }
 
+  ngOnDestroy() {
+    this.destroy$.next(null);
+  }
+
   private setupUser() {
     const userId = this.route.snapshot.params.userId;
 
     this.userService.getUserById(userId)
+      .pipe(
+        takeUntil(this.destroy$)
+      )
       .subscribe({
         next: (user) => {
           this.user = user;
